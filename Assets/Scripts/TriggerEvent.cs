06bd244a40365d744eaba0c1ef3d459341463063
Aidan Trimmer <aidan@trimmerfamily.com>
2024-10-31 15:19:00 -0400

    adding hitting ball physics

---
+++ Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class TriggerEvents : MonoBehaviour
+{
+    public UnityEvent onTrigger;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Ball"))
+        {
+            if (onTrigger != null)
+            {
+                onTrigger.Invoke();
+            }
+        }
+    }
+}
